37d94835c907a50c6eb952869bb4dbf740896a54
koala1101 <rproba@list.ru>
2022-03-04 20:03:53 +0300

    fixed controller

--- server/src/main/java/ru/itlab/server/controller/IntegerController.java
+++ server/src/main/java/ru/itlab/server/controller/IntegerController.java
@@ -1,17 +1,20 @@
 package ru.itlab.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itlab.server.service.RandomServiceImpl;
+
+import java.util.Map;
 
 @RestController
 public class IntegerController {
     @Autowired
     RandomServiceImpl randomService;
 
-    @RequestMapping("/getIntegers")
-    public String test(){
-        return randomService.getInteger(0,10, 5).toString();
+    @RequestMapping("/getIntegers/{min}/{max}/{count}")
+    public String test(@PathVariable Map<String, String> pathVarsMap){
+        return randomService.getInteger(Integer.parseInt(pathVarsMap.get("min")),Integer.parseInt(pathVarsMap.get("max")), Integer.parseInt(pathVarsMap.get("count"))).toString();
     }
 }
