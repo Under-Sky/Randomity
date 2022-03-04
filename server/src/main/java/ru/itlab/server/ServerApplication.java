a64254d0e0b13140d54f12824ae6824308eb9f13
koala1101 <rproba@list.ru>
2022-03-04 19:33:36 +0300

    getIntegersMethod ready

--- server/src/main/java/ru/itlab/server/ServerApplication.java
+++ server/src/main/java/ru/itlab/server/ServerApplication.java
@@ -8,9 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
