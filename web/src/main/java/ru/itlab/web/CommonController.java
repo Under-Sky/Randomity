fbf1ac79fac8660f5012799879b03b2ee179fe9c
angelina <evsikova@technokratos.com>
2022-03-19 14:18:55 +0300

    add ".html" to CommonController

--- web/src/main/java/ru/itlab/web/CommonController.java
+++ web/src/main/java/ru/itlab/web/CommonController.java
@@ -8,6 +8,6 @@ public class CommonController {
 
     @GetMapping("/registration")
     public String getSignInPage() {
-        return "registration";
+        return "registration.html";
     }
 }
